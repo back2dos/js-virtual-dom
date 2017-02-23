e6360b975a247dc2c7611de7d60ec0dba8bda567
back2dos <back2dos@gmail.com>
2017-02-23 14:41:14 +0100

    Key handling.

--- src/vdom/Attr.hx
+++ src/vdom/Attr.hx
@@ -13,16 +13,23 @@ abstract Ext(String) from String to String {
       else js.Lib.undefined;
 }
 
-abstract Key(Dynamic) from String from Int {
+abstract Key(Dynamic) from String from Int from Float from Bool {
   static var keygen = 0;
-  @:from static function ofAny<T>(v:T):Key {
-	  if (v == null) return null;
+
+  @:from static function ofObj<T:{}>(v:T):Key {
+    if (v == null) return null;
     var o: { __vdomKey__:Key } = cast v;
     if (o.__vdomKey__ == null) o.__vdomKey__ = keygen++;
     return o.__vdomKey__;
   }
-}
 
+  @:from static function ofAny<T>(v:T):Key 
+    return switch Type.typeof(v) {
+      case TInt, TFloat, TBool, TClass(String): cast v;
+      case TClass(Array): (cast v : Array<Dynamic>).join(':');
+      default: ofObj(cast v);
+    }
+}
 typedef Attr = AttrOf<Element>;
 
 @:forward
