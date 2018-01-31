fc72fe1c8b971892da004383c7d63e37e7741574
Kevin Leung <kevinresol@gmail.com>
2018-01-31 14:55:25 +0800

    Add script tag

--- src/vdom/VDom.hx
+++ src/vdom/VDom.hx
@@ -85,6 +85,7 @@ extern class VDom {
 
   static inline function select(attr: SelectAttr, ?children:Children):VNode return h('select', attr, children);
   static inline function option(attr: OptionAttr, ?children:Children):VNode return h('option', attr, children);
+  static inline function script(attr: ScriptAttr, ?children:Children):VNode return h('script', attr, children);
 
   static inline function raw(attr: vdom.HtmlFragment.RawAttr):VNode 
     return vdom.HtmlFragment.create(attr);
@@ -221,6 +222,14 @@ typedef OptionAttr = {> AttrOf<OptionElement>,
   @:optional var value(default, never):String;
   @:optional var text(default, never):String;
   @:optional var index(default, never):Int;
+}
+
+typedef ScriptAttr = {> AttrOf<ScriptElement>,
+  @:optional var async(default, never):Bool;
+  @:optional var charset(default, never):String;
+  @:optional var defer(default, never):Bool;
+  @:optional var src(default, never):String;
+  @:optional var type(default, never):String;
 }
 
 
