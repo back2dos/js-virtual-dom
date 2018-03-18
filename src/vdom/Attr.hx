e5f178e408c4c7237671e7151c1deba235f7968b
Juraj Kirchheim <back2dos@googlemail.com>
2018-03-18 10:40:36 +0100

    Fix event targets.

--- src/vdom/Attr.hx
+++ src/vdom/Attr.hx
@@ -62,9 +62,15 @@ typedef Attr = AttrOf<Element>;
 
 @:forward
 abstract EventFrom<E:Event, T:Element>(E) from E to E {
+  
   public var target(get, never):T;
     inline function get_target():T
+      return cast this.currentTarget;
+
+  public var exactTarget(get, never):Element;
+    inline function get_exactTarget():Element
       return cast this.target;
+  
 }
 
 typedef AttrOf<Target:Element> = {
